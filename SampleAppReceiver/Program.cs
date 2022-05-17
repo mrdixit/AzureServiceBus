c37d8c0f5f9e9f35a6e65f29ae8029491ebc6266
Mahesh.Dixit <mahesh.dixit@siemensgamesa.com>
2022-05-17 20:56:24 +0530

    Receiver code

--- SampleAppReceiver/Program.cs
+++ SampleAppReceiver/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using System.Text.Json;
+using Microsoft.Azure.ServiceBus;
+using SampleShared;
+
+namespace SampleAppReceiver
+{
+    class Program
+    {
+        const string connString = "";
+        static IQueueClient? qClient;
+
+        static async Task Main(string[] args)
+        {
+            qClient = new QueueClient(connString, "personqueue");
+
+            var msgOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {   
+                // How many messages we can process at time
+                MaxConcurrentCalls = 1,
+                // need to wait until a message is fully processed
+                AutoComplete = false,
+            };
+
+            qClient.RegisterMessageHandler(ProcessMessageAsync, msgOptions);
+
+            Console.ReadLine();
+            await qClient.CloseAsync();
+        }
+
+        private static async Task ProcessMessageAsync(Message msg, CancellationToken token)
+        {
+            // Deserialisr the msg body
+            var jsonBody = Encoding.UTF8.GetString(msg.Body);
+            var personObj = JsonSerializer.Deserialize<Person>(jsonBody);
+
+            Console.WriteLine($"First Name: {personObj?.FirstName}");
+            Console.WriteLine($"Last Name: {personObj?.LastName}");
+            Console.WriteLine($"Email: {personObj?.Email}");
+
+            // Updating the queue that the message has been processed successfully
+            await qClient.CompleteAsync(msg.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine($"Something went wrong, {args.Exception}");
+            return Task.CompletedTask;
+        }
+    }
+}
--- SampleWebSender/Controllers/HomeController.cs
+++ SampleWebSender/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SampleShared;
 using SampleWebSender.Models;
+using SampleWebSender.Services;
 
 namespace SampleWebSender.Controllers;
 
@@ -8,13 +10,24 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IAzureBusService azureBusService;
+
+    public HomeController(ILogger<HomeController> logger, IAzureBusService busService)
     {
         _logger = logger;
+        this.azureBusService = busService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(Person person)
+    {
+        await azureBusService.SendMessageAsync(person, "AzureQueueNameHere");
+        return RedirectToAction("Privacy");
     }
 
     public IActionResult Index()
     {
+
         return View();
     }
 
